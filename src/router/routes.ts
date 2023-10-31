4035df3632b43dde69b867a150886fa9f3e9b2e3
Joan Galmés Riera <joan.galmes@gmail.com>
2023-10-31 09:18:30 +0100

    canviada la pantalla inicial de convalidacions

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,7 +5,7 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '', component: () => import('pages/SolicitudList.vue') },
       { path: 'categories', component: () => import('pages/CategoriaList.vue') },
       { path: 'categoria', component: () => import('pages/CategoriaForm.vue') },
       { path: 'categoria/:id', component: () => import('pages/CategoriaForm.vue') },
